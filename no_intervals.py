da8fce532dd1b5a171880940ed19a8240a11df54
ripcord <jfhead97@gmail.com>
2018-11-29 10:29:37 -0500

    Removed if/else

--- no_intervals.py
+++ no_intervals.py
@@ -69,104 +69,12 @@ else:
 #print(ELITE_STEP_SIZE)
 
 
-if FITNESS_EVALS:
-    evals = 0
-    while evals < FITNESS_EVALS:
-        #CREATION OF THE ARRAY OF X POINTS ON N DIMENSIONS => DATASET
-        #Creating the "followers"
-        DATASET = np.full((NUMBER_OF_POINTS, NUMBER_OF_DIMENSIONS), None)
+if not FITNESS_EVALS:
+    FITNESS_EVALS = 1
 
+evals = 0
 
-        # for INDEX, VALUE in ITERABLE:
-        for point , i in enumerate(DATASET):
-            for dim, x in enumerate(i):
-                DATASET[point][dim] = random.uniform(BOUNDS[0], BOUNDS[1])
-        #print("--*FULL DATASET BEFORE:\n",DATASET)
-
-
-        #Meat of the program, Merge/Fitness Function portion of the program
-        runs = 1
-
-        ELITE_DATASET = DATASET[0]
-#        ELITE_PREVIOUS = copy.deepcopy(ELITE_DATASET)
-#        while True:
-        while runs <= ITERS:
-            ELITE_STEP_SIZE = ((0.8/math.log(NUMBER_OF_DIMENSIONS)) * (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
-            STEP_SIZE = ((1.0/math.log(NUMBER_OF_DIMENSIONS)) *  (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
-
-            #Determine best dataset => ELITE_DATASET
-            # 1 point per dimension. there can only be 1 best
-            for i in DATASET:
-                #print(i, fitness(i))
-                if fitness(i) > fitness(ELITE_DATASET):
-                    #print("Old Best:", fitness(ELITE_DATASET), "New Best:", fitness(i))
-                    ELITE_DATASET = copy.deepcopy(i[:])
-
-            prev_fitness = fitness(ELITE_DATASET)
-
-            #Find the midpoint of all the points
-            DIMENSIONAL_MIDPOINT = np.full((NUMBER_OF_DIMENSIONS, 1), None)
-            for index, i in enumerate(DATASET.T):
-                DIMENSIONAL_MIDPOINT[index] = np.mean(i)
-            #print("THE MIDPOINT", DIMENSIONAL_MIDPOINT)
-
-
-            #random dimensions to change
-            dimensions_to_change = random.sample(range(0, NUMBER_OF_DIMENSIONS), random.randint(0, NUMBER_OF_DIMENSIONS))
-            #print(dimensions_to_change)
-            
-            #move in those specific random dimensions toward middle
-            for point, x in enumerate(DATASET):
-                #print(point, ":DATASET:", DATASET[point], "\nELITE_DATASET", ELITE_DATASET)
-                if fitness(DATASET[point]) >= fitness(ELITE_DATASET):
-                    
-                    ELITE_DATASET = copy.deepcopy(DATASET[point])
-
-                    #Hill climbing for the elite point
-                    for climb in range(ELITE_CLIMBS):
-                        for i in range(NUMBER_OF_DIMENSIONS):
-                            TEMP = copy.deepcopy(DATASET[point])
-                            
-                            DATASET[point][i] += random.uniform(0,ELITE_STEP_SIZE)
-                            
-                            if not (fitness(DATASET[point]) > fitness(TEMP)):
-                                for x in range(NUMBER_OF_DIMENSIONS):
-                                    DATASET[point][x] = TEMP[x]
-                            
-                                DATASET[point][i] -= random.uniform(0,ELITE_STEP_SIZE)
-                                
-                                if not (fitness(DATASET[point]) > fitness(TEMP)):
-                                    for x in range(NUMBER_OF_DIMENSIONS):
-                                        DATASET[point][x] = TEMP[x]
-                                #if(fitness(DATASET[point]) > fitness(ELITE_DATASET)):
-                                    #print("HILLCLIMBING => Old Best:", fitness(DATASET[point]), "New Best:", fitness(ELITE_DATASET)) 
-                            ELITE_DATASET = copy.deepcopy(DATASET[point]) #CRITICAL LINE!
-
-                else:
-                    for i in dimensions_to_change:
-                        if(x[i] < DIMENSIONAL_MIDPOINT[i]):
-                            DATASET[point][i] += random.uniform(0, STEP_SIZE)
-                            #DATASET[point][i] += STEP_SIZE
-                        if(x[i] > DIMENSIONAL_MIDPOINT[i]):
-                            DATASET[point][i] -= random.uniform(0, STEP_SIZE)
-                            #DATASET[point][i] -= STEP_SIZE
-                #print(DATASET)
-
-            runs += 1
-
-            #if(prev_fitness < fitness(ELITE_DATASET)):
-            #    print(runs, fitness(ELITE_DATASET))
-
-        evals += 1
-
-        #print("--*FULL DATASET AFTER:\n", DATASET)
-        #print("--*DIMENSION MIDPOINT:\n", DIMENSIONAL_MIDPOINT)
-        #print("--*BEST DATA:", ELITE_DATASET, "{:>2}" .format(" "), "|","{:>2}" .format(" "),"FITNESS:", fitness(ELITE_DATASET))
-        print("{s1:<{width}} {s2}".format(s1="--*BEST DATA: " + str(ELITE_DATASET), width=10 * ((NUMBER_OF_DIMENSIONS + 20)//2), s2="| FITNESS: " + str(fitness(ELITE_DATASET)) ))
-        #print("--*BEST DATA:", ELITE_DATASET, "| FITNESS:", fitness(ELITE_DATASET))
-        #print("{s1} {s2:>{width}}".format(s1="--*BEST DATA: " + str(ELITE_DATASET), width=30, s2="| FITNESS: " + str(fitness(ELITE_DATASET)) ))
-
-else:
+while evals < FITNESS_EVALS:
     #CREATION OF THE ARRAY OF X POINTS ON N DIMENSIONS => DATASET
     #Creating the "followers"
     DATASET = np.full((NUMBER_OF_POINTS, NUMBER_OF_DIMENSIONS), None)
@@ -183,8 +91,8 @@ else:
     runs = 1
 
     ELITE_DATASET = DATASET[0]
-#    ELITE_PREVIOUS = copy.deepcopy(ELITE_DATASET)
-#    while True:
+#        ELITE_PREVIOUS = copy.deepcopy(ELITE_DATASET)
+#        while True:
     while runs <= ITERS:
         ELITE_STEP_SIZE = ((0.8/math.log(NUMBER_OF_DIMENSIONS)) * (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
         STEP_SIZE = ((1.0/math.log(NUMBER_OF_DIMENSIONS)) *  (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
@@ -252,12 +160,17 @@ else:
         #if(prev_fitness < fitness(ELITE_DATASET)):
         #    print(runs, fitness(ELITE_DATASET))
 
+        if (runs % 10) == 0:
+            if (fitness(ELITE_DATASET) - prev_fitness) < 0.00009:
+                #break
+                runs = ITERS + 1
+
+    evals += 1
+
     #print("--*FULL DATASET AFTER:\n", DATASET)
     #print("--*DIMENSION MIDPOINT:\n", DIMENSIONAL_MIDPOINT)
     #print("--*BEST DATA:", ELITE_DATASET, "{:>2}" .format(" "), "|","{:>2}" .format(" "),"FITNESS:", fitness(ELITE_DATASET))
-    #print("{s1:<{width}} {s2}".format(s1="--*BEST DATA: " + str(ELITE_DATASET), width=10 * ((NUMBER_OF_DIMENSIONS + 15)//2), s2="| FITNESS: " + str(fitness(ELITE_DATASET)) ))
+    print("{s1:<{width}} {s2}".format(s1="--*BEST DATA: " + str(ELITE_DATASET), width=10 * ((NUMBER_OF_DIMENSIONS + 20)//2), s2="| FITNESS: " + str(fitness(ELITE_DATASET)) ))
     #print("--*BEST DATA:", ELITE_DATASET, "| FITNESS:", fitness(ELITE_DATASET))
-    print("{s1} {s2:>{width}}".format(s1="--*BEST DATA: " + str(ELITE_DATASET), width=30, s2="| FITNESS: " + str(fitness(ELITE_DATASET)) ))
-    
-    print(len(ELITE_DATASET))
+    #print("{s1} {s2:>{width}}".format(s1="--*BEST DATA: " + str(ELITE_DATASET), width=30, s2="| FITNESS: " + str(fitness(ELITE_DATASET)) ))
 
