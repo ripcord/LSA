118d8ab08aa295764a5a0e2b2766db0165be4c8c
ripcord <jfhead97@gmail.com>
2018-12-02 09:24:00 -0500

    Support for finding minima and maxima

--- no_intervals.py
+++ no_intervals.py
@@ -29,6 +29,8 @@ STAGNANT_RUN_CAP = 100          #Maximum number of allowable runs for which the 
 RESULTS = {}                    #Set of evaluation result information (actual iterations, fitness, elite coordinates, evaluation number)
 FITNESS_EVALS = None            #Number of fitness evaluations (optional)
 OUTPUT_FILE = None              #Handler for output file (optional)
+FITNESS_MIN = False             #Switch to enable finding global minimum rather than maximum
+
 
 #Fitness function "Elvis Needs Boats"
 #2D optimum = ~0.41
@@ -42,8 +44,32 @@ def elvis_needs_boats(data):
     fitness = -math.sqrt(temp) + math.sin(sin_temp)
     return fitness
 
+#Fitness function "Rastrigin"
+#Global minimum at "f(0,...,0) = 0"
+def rastrigin(data):
+    fitness = 0.0
+    for i in range(0, NUMBER_OF_DIMENSIONS):
+        fitness += (data[i]**2) - 10 * math.cos(2 * math.pi * data[i])
+    fitness += (10 * NUMBER_OF_DIMENSIONS)
+    return fitness
+
 def fitness(data):
     return elvis_needs_boats(data)
+    #return rastrigin(data)
+
+#Compares fitness of two values "a" and "b"
+def comp_fitness(a, b, equals=None):
+    global FITNESS_MIN
+    if FITNESS_MIN:
+        if equals:
+            return (fitness(a) <= fitness(b))
+        else:
+            return (fitness(a) < fitness(b))
+    else:
+        if equals:
+            return (fitness(a) >= fitness(b))
+        else:
+            return (fitness(a) > fitness(b))
 
 #Returns a step size, variable for elite and non-elite points
 def get_step(runs, elite=False, nd_offset=None):
@@ -57,12 +83,12 @@ def get_step(runs, elite=False, nd_offset=None):
             return ((0.8/math.log(NUMBER_OF_DIMENSIONS)) * (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
     else:
         if nd_offset:
-            return ((1.0/math.log(NUMBER_OF_DIMENSIONS + nd_offset)) *  (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
+            return ((1.0/math.log(NUMBER_OF_DIMENSIONS + nd_offset)) * (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
         else:
-            return ((1.0/math.log(NUMBER_OF_DIMENSIONS)) *  (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
+            return ((1.0/math.log(NUMBER_OF_DIMENSIONS)) * (NUMBER_OF_POINTS/runs*4)) % BOUNDS[1]
 
 #Output to a file and/or STDOUT
-def output(evaluations, comp_runs, l_runtime, out_file=None):
+def output(evaluations, comp_runs, stag_runs, l_runtime, out_file=None):
     global ELITE_DATASET
     global ITERS
     global STAGNANT_RUN_CAP
@@ -70,15 +96,15 @@ def output(evaluations, comp_runs, l_runtime, out_file=None):
         out_file.write("\n*-->Evaluation #{}:\n".format(evaluations))
         out_file.write("\tElite Point Coordinates: {}\n".format(ELITE_DATASET))
         out_file.write("\t{:>25}{}\n\t{:>25}{}\n\t{:>25}{}\n\t{:>25}{}\n".format("Maximum Iterations: ", ITERS,\
-            "Completed Iterations: ", completed_runs, "Stagnant Iterations: ", STAGNANT_RUN_CAP, "Actual Iterations: ",\
-            completed_runs - STAGNANT_RUN_CAP))
-        out_file.write("\n\t{:>25}{:.5f} sec\n".format("Approximate Runtime: ", runtime))
+            "Completed Iterations: ", comp_runs, "Stagnant Iterations: ", stag_runs, "Actual Iterations: ",\
+            comp_runs - stag_runs))
+        out_file.write("\n\t{:>25}{:.5f} sec\n".format("Approximate Runtime: ", l_runtime))
         out_file.write("\n\t{:>24} {} ***\n".format("*** FITNESS:", fitness(ELITE_DATASET)))
     else:
-        print("{}".format("*-->Evaluation #" + str(evaluations)) + ":")
-        print("\t{}".format("Elite Point Coordinates: " + str(ELITE_DATASET)))
+        print("*-->Evaluation #{}:".format(evaluations))
+        print("\tElite Point Coordinates: {}".format(ELITE_DATASET))
         print("\t{:>25}{}\n\t{:>25}{}\n\t{:>25}{}\n\t{:>25}{}".format("Maximum Iterations: ", ITERS, "Completed Iterations: ", comp_runs,\
-            "Stagnant Iterations: ", STAGNANT_RUN_CAP, "Actual Iterations: ", comp_runs - STAGNANT_RUN_CAP))
+            "Stagnant Iterations: ", stag_runs, "Actual Iterations: ", comp_runs - stag_runs))
         print("\n\t{:>24} {:.5f} sec\n".format("Approximate Runtime:", l_runtime))
         print("\t{:>25}{} ***\n".format("*** FITNESS: ",fitness(ELITE_DATASET)))
 
@@ -97,6 +123,9 @@ def file_prep():
                 if re.search('Trial', line, re.I):
                     t_num = re.search(r'\d+',line.rstrip()).group()
                     break
+    else:
+        print("File '", OUTPUT_FILE, "' does not exist")
+        exit()
     OUTPUT_FILE = open(OUTPUT_FILE, "a+")
     OUTPUT_FILE.write("\n\n<{}>\n{:^80}\nDate: {}".format("+" * 90, "<- Trial #" + str(int(t_num) + 1) + " ->",\
         str(datetime.datetime.now().strftime("%Y-%m-%d"))))
@@ -107,12 +136,18 @@ def file_prep():
 
 #Writes the best fitness to STDOUT and/or an output file
 def print_best(data, out_file=None):
+    global FITNESS_MIN
     i = 1
-    temp = data[1][0]
+    temp = data[i][0]
     for k in sorted(data.keys()):
-        if data[k][0] > temp:
-            temp = data[k][0]
-            i = k
+        if FITNESS_MIN:
+            if data[k][0] < temp:
+                temp = data[k][0]
+                i = k
+        else:
+            if data[k][0] > temp:
+                temp = data[k][0]
+                i = k
     print("\n\t{0:>19}\n\t{1:>19}\n\t{0:>19}".format("-" * 18,"| Optimum Result |"))
     print("{:>18} {}\nActual Iterations: {}\nElite Coordinates: {}\n{:>18} {}".format("Best Evaluation:", i, data[i][2],\
             data[i][1], "Fitness:", temp))
@@ -122,47 +157,87 @@ def print_best(data, out_file=None):
             i, data[i][2], data[i][1], "Fitness:", temp))
     return
 
+def print_usage():
+    print("\nNote: The argument 'OPTIMUM TYPE' accepts values of '+' or '-'. Value of '+' will produce global maxima, while \
+'-' will produce global minima.")
+    print("\nUse cases:\n\t\"python", sys.argv[0], "[OPTIMUM TYPE (max = '+', min = '-')] [DIMENSIONS] [POINTS]\"")
+    print("\n\t\"python", sys.argv[0], "[OPTIMUM TYPE (max = '+', min = '-')] [DIMENSIONS] [POINTS] [FITNESS EVALUATIONS]\"")
+    print("\n\t\"python", sys.argv[0], "[OPTIMUM TYPE (max = '+', min = '-')] [DIMENSIONS] [POINTS] [FITNESS EVALUATIONS] \
+[PATH TO OUTPUT FILE]\"")
+
+
 #Get arguments from cmd, they go as follows {arg1: X points, arg2: N dimensions}
 #X points on N dimensions
-if (len(sys.argv) == 3 ):
+if ("?" in sys.argv) or (len(sys.argv) == 0):
+    print_usage()
+    exit()
+if (len(sys.argv) == 4):
     try:
-        sys.argv[1] = int(sys.argv[1])
-        sys.argv[2] = int(sys.argv[2])
-    except ValueError:
-        print("Arguments must be of type integer")
-        exit()
-    NUMBER_OF_DIMENSIONS = sys.argv[1]
-    NUMBER_OF_POINTS = sys.argv[2]
-elif (len(sys.argv) == 4):
-    try:
-        sys.argv[1] = int(sys.argv[1])
         sys.argv[2] = int(sys.argv[2])
         sys.argv[3] = int(sys.argv[3])
     except ValueError:
-        print("Arguments must be of type integer")
+        print("Number of dimensions and points must be of type integer")
         exit()
-    NUMBER_OF_DIMENSIONS = sys.argv[1]
-    NUMBER_OF_POINTS = sys.argv[2]
-    FITNESS_EVALS = sys.argv[3]
+    NUMBER_OF_DIMENSIONS = sys.argv[2]
+    NUMBER_OF_POINTS = sys.argv[3]
+    if (NUMBER_OF_DIMENSIONS <= 0) or (NUMBER_OF_POINTS <= 0):
+        print("Number of dimensions and points must be greater than 0")
+        exit()
+    if sys.argv[1] == "+":
+        FITNESS_MIN = False
+    elif sys.argv[1] == "-":
+        FITNESS_MIN = True
+    else:
+        print("Invalid optimum type. Acceptable values: '+' (maximum), '-' (minimum)")
+        exit()
 elif (len(sys.argv) == 5):
     try:
-        sys.argv[1] = int(sys.argv[1])
         sys.argv[2] = int(sys.argv[2])
         sys.argv[3] = int(sys.argv[3])
+        sys.argv[4] = int(sys.argv[4])
     except ValueError:
-        print("Invalid argument types")
+        print("Number of dimensions, points and fitness evaluations must be of type integer")
         exit()
-    NUMBER_OF_DIMENSIONS = sys.argv[1]
-    NUMBER_OF_POINTS = sys.argv[2]
-    FITNESS_EVALS = sys.argv[3]
-    OUTPUT_FILE = sys.argv[4]
+    NUMBER_OF_DIMENSIONS = sys.argv[2]
+    NUMBER_OF_POINTS = sys.argv[3]
+    FITNESS_EVALS = sys.argv[4]
+    if (NUMBER_OF_DIMENSIONS <= 0) or (NUMBER_OF_POINTS <= 0) or (FITNESS_EVALS <= 0):
+        print("Number of dimensions, points and fitness evaluations must be greater than 0")
+        exit()
+    if sys.argv[1] == "+":
+        FITNESS_MIN = False
+    elif sys.argv[1] == "-":
+        FITNESS_MIN = True
+    else:
+        print("Invalid optimum type. Acceptable values: '+' (maximum), '-' (minimum)")
+        exit()
+elif (len(sys.argv) == 6):
+    try:
+        sys.argv[2] = int(sys.argv[2])
+        sys.argv[3] = int(sys.argv[3])
+        sys.argv[4] = int(sys.argv[4])
+    except ValueError:
+        print("Number of dimensions, points and fitness evaluations must be of type integer")
+        exit()
+    NUMBER_OF_DIMENSIONS = sys.argv[2]
+    NUMBER_OF_POINTS = sys.argv[3]
+    FITNESS_EVALS = sys.argv[4]
+    OUTPUT_FILE = sys.argv[5]
     if os.path.isdir(OUTPUT_FILE):
         print("File '", os.path.abspath(OUTPUT_FILE), "' is a directory")
         exit()
+    if (NUMBER_OF_DIMENSIONS <= 0) or (NUMBER_OF_POINTS <= 0) or (FITNESS_EVALS <= 0):
+        print("Number of dimensions, points and fitness evaluations must be greater than 0")
+        exit()
+    if sys.argv[1] == "+":
+        FITNESS_MIN = False
+    elif sys.argv[1] == "-":
+        FITNESS_MIN = True
+    else:
+        print("Invalid optimum type. Acceptable values: '+' (maximum), '-' (minimum)")
+        exit()
 else:
-    print("Usage: \"python", sys.argv[0], "[NUMBER OF DIMENSIONS] [NUMBER OF POINTS]\"")
-    print("OR\nUsage: \"python", sys.argv[0], "[NUMBER OF DIMENSIONS] [NUMBER OF POINTS] [NUMBER OF FITNESS EVALUATIONS]\"")
-    print("OR\nUsage: \"python", sys.argv[0], "[NUMBER OF DIMENSIONS] [NUMBER OF POINTS] [NUMBER OF FITNESS EVALUATIONS] [PATH TO OUTPUT FILE]\"")
+    print_usage()
     exit()
 
 
@@ -170,7 +245,6 @@ else:
 #STEP_SIZE = 1/math.log(NUMBER_OF_DIMENSIONS)
 #print(STEP_SIZE)
 #print(ELITE_STEP_SIZE)
-
 
 if not FITNESS_EVALS:
     FITNESS_EVALS = 1
@@ -202,6 +276,8 @@ while evals < FITNESS_EVALS:
     runs = 1
     completed_runs = 0
     stagnant_runs = 0
+    stag = 0
+    runtime = 0.0
 
    
     ELITE_DATASET = DATASET[0]
@@ -226,7 +302,11 @@ while evals < FITNESS_EVALS:
         # 1 point per dimension. there can only be 1 best
         for i in DATASET:
             #print(i, fitness(i))
-            if fitness(i) > fitness(ELITE_DATASET):
+
+####
+    #        if fitness(i) > fitness(ELITE_DATASET):
+            if comp_fitness(i, ELITE_DATASET):
+####
                 #print("Old Best:", fitness(ELITE_DATASET), "New Best:", fitness(i))
                 ELITE_DATASET = copy.deepcopy(i[:])
 
@@ -253,9 +333,11 @@ while evals < FITNESS_EVALS:
         for point, x in enumerate(DATASET):
             #print(point, ":DATASET:", DATASET[point], "\nELITE_DATASET", ELITE_DATASET)
 
+###
             #Elite Point!
-            if fitness(DATASET[point]) >= fitness(ELITE_DATASET):
-                
+    #        if fitness(DATASET[point]) >= fitness(ELITE_DATASET):
+            if comp_fitness(DATASET[point], ELITE_DATASET, "="):
+ ###               
                 ELITE_DATASET = copy.deepcopy(DATASET[point])
 
                 #Hill climbing for the elite point
@@ -265,13 +347,19 @@ while evals < FITNESS_EVALS:
                         
                         DATASET[point][i] += random.uniform(0,ELITE_STEP_SIZE)
                         
-                        if not (fitness(DATASET[point]) > fitness(TEMP)):
+###
+   #                     if not (fitness(DATASET[point]) > fitness(TEMP)):
+                        if not comp_fitness(DATASET[point], TEMP):
+###
                             for x in range(NUMBER_OF_DIMENSIONS):
                                 DATASET[point][x] = TEMP[x]
                         
                             DATASET[point][i] -= random.uniform(0,ELITE_STEP_SIZE)
                             
-                            if not (fitness(DATASET[point]) > fitness(TEMP)):
+###
+   #                         if not (fitness(DATASET[point]) > fitness(TEMP)):
+                            if not comp_fitness(DATASET[point], TEMP):
+###
                                 for x in range(NUMBER_OF_DIMENSIONS):
                                     DATASET[point][x] = TEMP[x]
                             #if(fitness(DATASET[point]) > fitness(ELITE_DATASET)):
@@ -297,13 +385,21 @@ while evals < FITNESS_EVALS:
         #if(prev_fitness < fitness(ELITE_DATASET)):
             #print(runs, fitness(ELITE_DATASET), ELITE_STEP_SIZE)
 
-        if ((fitness(ELITE_DATASET) - prev_fitness) <= 0.0):
-            stagnant_runs += 1
-            if stagnant_runs == STAGNANT_RUN_CAP:
-                completed_runs = runs
-                stagnant_runs = 0
-                runtime = timeit.default_timer() - start_time
-                runs = ITERS + 1
+###
+   #     if ((fitness(ELITE_DATASET) - prev_fitness) <= 0.0):
+        if FITNESS_MIN:
+            if ((fitness(ELITE_DATASET) - prev_fitness) >= 0.0):
+                stag += 1
+        else:
+            if ((fitness(ELITE_DATASET) - prev_fitness) <= 0.0):
+                stag += 1
+        if stag == STAGNANT_RUN_CAP:
+            completed_runs = runs
+            stagnant_runs = stag
+            stag = 0
+            runtime = timeit.default_timer() - start_time
+            runs = ITERS + 1
+###
 
         #if (runs % 10) == 0:
         #    print(fitness(ELITE_DATASET), prev_fitness, fitness(ELITE_DATASET) - prev_fitness)
@@ -311,19 +407,23 @@ while evals < FITNESS_EVALS:
         #        optimum_runs = runs
         #        runs = ITERS + 1
 
+    if stagnant_runs == 0:
+        completed_runs = runs - 1
+        runtime = timeit.default_timer() - start_time
+
     evals += 1
 
     #This needs to be after "evals += 1" !!!
-    RESULTS[evals] = (fitness(ELITE_DATASET), ELITE_DATASET, completed_runs - STAGNANT_RUN_CAP)
+    RESULTS[evals] = (fitness(ELITE_DATASET), ELITE_DATASET, completed_runs - stagnant_runs)
 
     #print("--*FULL DATASET AFTER:\n", DATASET)
     #print("--*DIMENSION MIDPOINT:\n", DIMENSIONAL_MIDPOINT)
     #print("--*BEST DATA:", ELITE_DATASET, "| FITNESS:", fitness(ELITE_DATASET))
  
     if OUTPUT_FILE:
-        output(evals, completed_runs, runtime, OUTPUT_FILE)
+        output(evals, completed_runs, stagnant_runs, runtime, OUTPUT_FILE)
 
-    output(evals, completed_runs, runtime)
+    output(evals, completed_runs, stagnant_runs, runtime)
     #print("\tRuns:", optimum_runs)
 
 if OUTPUT_FILE:
